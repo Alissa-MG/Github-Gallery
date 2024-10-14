c3c28b0c9fc7ace979f19a77f150841d311ffd9e
Alissa-MG <alissagarcia10118@gmail.com>
2024-10-14 16:58:46 -0500

    properties-needed

--- js/script.js
+++ js/script.js
@@ -6,23 +6,25 @@ const username = "Alissa-MG";
 const gitUserInfo = async function () {
     const userInfo =await fetch(`https://api.github.com/users/${username}`);
     const data = await userInfo.json();
-    displayUserInfo(data);
-    console.log(date);
+    //displayUserInfo(data);
+    console.log(data);
 };
 
 gitUserInfo();
+
 
 const displayUserInfo = function (data) {
     const newDiv = document.createElement("div");
     newDiv.classList.add("user-info");
     newDiv.innerHTML = `
         <figure>
-            <img alt="user avatar" src=${} />
+            <img alt="user avatar" src=${data.avatar_url} />
         </figure>
     <div>
-      <p><strong>Name:</strong> ${}</p>
-      <p><strong>Bio:</strong> ${}</p>
-      <p><strong>Location:</strong> ${}</p>
-      <p><strong>Number of public repos:</strong> ${}</p>
+      <p><strong>Name:Alissa Garcia</strong> ${data.name}</p>
+      <p><strong>Bio:I am a new mom looking to leave my full-time job for a skill that will alow me to spend more time with my twins. </strong> ${data.bio}</p>
+      <p><strong>Location:South Texas</strong> ${data.location}</p>
+      <p><strong>Number of public repos:23</strong> ${data.public_repos}</p>
     </div>`
+    overview.append(newDiv);
 };
